f31bc76955dc02e6d1cef6b3e9c5c60a2e3680c8
navins94 <nikshingote@gmail.com>
2023-06-19 02:12:17 +0530

    Change eslint version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
 
-import '@/styles/index.css'
+import '@/styles/index.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
